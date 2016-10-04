bd1d621feedc2be2194ba83126e3a78900b28f53
Radivarig <reslav.hollos@gmail.com>
2016-10-04 23:03:51 +0000

    (add) preserving state

--- www/src/entry.js
+++ www/src/entry.js
@@ -1,11 +1,24 @@
+import 'react-hot-loader/patch'
 import 'babel-polyfill'
-import React from'react'
-import ReactDOM from'react-dom'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { AppContainer } from 'react-hot-loader'
 
 import initGlobalReduxStore from './initGlobalReduxStore.jsx'
 initGlobalReduxStore()
 
-import routes from'./routes.jsx'
+import routes from './routes.jsx'
 
-ReactDOM.render(routes, document.getElementById('app'))
+const renderRoutes = () => {
+    const routes = require('./routes.jsx')
+    const container = <AppContainer>{routes}</AppContainer>
+    ReactDOM.render(container, document.getElementById('app'))
+}
+
+renderRoutes()
+
+if (module.hot)
+  module.hot.accept('./routes.jsx', renderRoutes)
 
