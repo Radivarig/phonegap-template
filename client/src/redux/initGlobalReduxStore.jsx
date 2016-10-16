99c5b062f232e5fe916511bd882710c23cd7dc74
Radivarig <reslav.hollos@gmail.com>
2016-10-16 20:52:12 +0000

    (rm) unused var

--- client/src/redux/initGlobalReduxStore.jsx
+++ client/src/redux/initGlobalReduxStore.jsx
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import type { storeStateType } from 'types'
 import { ajax_post } from './server/server_api.js'
 
 import reducer from './redux/reducers/reducer'
