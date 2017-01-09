11cb74d0327cb1edea03da4fd4c15c1e6e8a9bf7
Radivarig <reslav.hollos@gmail.com>
2017-01-09 22:20:50 +0000

    (fix) updated react-router, added route example

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import { Router, Route, hashHistory } from 'react-router'
 import App from 'App'
 
+const ExampleRoute = ({params}) =>
+  <div>Example route with param: {params.param}.</div>
+
+const NotFoundRoute = () => <div>Route not found.</div>
+
 module.exports = (
   <Router history={hashHistory}>
     <Route path='/' component={App} />
+    <Route path='/example/:param' component={ExampleRoute} />
+    <Route path='*' component={NotFoundRoute} />
   </Router>
 )
