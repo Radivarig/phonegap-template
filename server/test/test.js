55766ceb02111cfca5749c463442bc48ee8c221b
Radivarig <reslav.hollos@gmail.com>
2017-01-30 06:31:01 +0000

    (add) npm run test:watch

--- server/test/test.js
+++ server/test/test.js
@@ -2,17 +2,11 @@ const chai = require('chai')
 const should = chai.should()
 const chaiHttp = require('chai-http')
 chai.use(chaiHttp)
-
 const app = require('../src/app.js')
-const server = app.listen(app.get('port'), () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server listening on port ${app.get('port')}`)
-})
-
 
 describe('test example', () => {
   it('POST /ajax_post should return 200', (done) => {
-    chai.request(server)
+    chai.request(app.listen())
     .post('/ajax_post')
     .end((err, res) => {
       res.statusCode.should.equal(200)
