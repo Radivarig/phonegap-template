5746396d0354fdf2026ebd2fb87392687cc2da00
Radivarig <reslav.hollos@gmail.com>
2017-02-01 01:37:25 +0000

    (chg) moved beforeEach afterEach out of describe

--- server/test/test.js
+++ server/test/test.js
@@ -4,30 +4,24 @@ const chai = require('chai')
 const expect = chai.expect
 const chaiHttp = require('chai-http')
 chai.use(chaiHttp)
-const app = require('../src/app.js')
 
 const knex = require('../db/knex.js')
-
 const {dbHandler} = require ('../src/dbHandler.js')
 const {loginHandler} = require ('../src/loginHandler.js')
 
+const app = require('../src/app.js')
+
+// reset databases before and after each test
+beforeEach(async () =>
+  await knex.migrate.rollback()
+  .then(() => knex.migrate.latest())
+  .then(() => knex.seed.run())
+)
+afterEach(async () => await knex.migrate.rollback())
+
+const email = 'testuser@test.com'
+
 describe('API loginHandler.js', () => {
-
-  beforeEach(async () =>
-    await knex.migrate.rollback()
-    .then(() =>
-      knex.migrate.latest()
-      .then(() =>
-        knex.seed.run()
-      )
-    )
-  )
-
-  afterEach(async () =>
-    await knex.migrate.rollback()
-  )
-
-  const email = 'testuser@test.com'
 
   it('`handleIfUnregisteredUser` should insert new user to table `unregistered`', async () => {
     const id_from_users = await dbHandler.getColumn ('id', 'users', {email})
@@ -41,4 +35,3 @@ describe('API loginHandler.js', () => {
   })
 
 })
-
