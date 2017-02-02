3dfbc35fb06df16469809fb7f2f5f63f4df83847
Radivarig <reslav.hollos@gmail.com>
2017-02-02 19:29:49 +0000

    (add) log process.env.NODE_ENV

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app.js')
 
 app.listen(app.get('port'), () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server listening on port ${app.get('port')}`)
+  console.log(`Server listening on port ${app.get('port')}`) // eslint-disable-line
+  console.log(`NODE_ENV=${process.env.NODE_ENV || ''}`) // eslint-disable-line
 })
